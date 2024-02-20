69cc11196841193d0d40ed689084ee1fb0e0b5d3
Thomas Mumena <mumenathomas@gmail.com>
2024-02-20 17:29:27 +0200

    Check if user input is a valid state. If correct, go to correct point on the map and write the name of the state. loop through this process until all states are filled in. Keep track of the user score.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import turtle
+import turtle, pandas
 
 screen = turtle.Screen()
 screen.title('U.S. States Game')
@@ -7,8 +7,36 @@ img = 'blank_states_img.gif'
 turtle.addshape(img)
 turtle.shape(img)
 
-answer = screen.textinput(title='Guess State Name', prompt='What\'s another state\'s name?')
-print(answer)
+# Extract states into a list
+data = pandas.read_csv('./50_states.csv')
+all_states_data = data['state']
+states_data_list = all_states_data.to_list()
+
+tim = turtle.Turtle()
+tim.hideturtle()
+tim.penup()
+
+num_states = 50
+score = 0
+states_guessed = []
+
+while len(states_guessed) < num_states:
+    # Get input from user and capitalize
+    answer = screen.textinput(title=f'{score}/{num_states}', prompt='What\'s another state\'s name?')
+    answer = answer.capitalize()
+
+    # pull the x and y coordinates of the state and write to the map
+    # is_in_list = answer in states_data_list
+    if answer in states_data_list:
+        state_info = data[data['state'] == answer]
+        x_cor = int(state_info['x'])
+        y_cor = int(state_info['y'])
+        tim.goto(x_cor, y_cor)
+        state_name = state_info['state'].item()
+        # print(state_name)
+        tim.write(arg=state_name, align='center', font=('Courier', 8, 'normal'))
+        states_guessed.append(answer)
+        score += 1
 
 # screen.exitonclick()
 turtle.mainloop()
